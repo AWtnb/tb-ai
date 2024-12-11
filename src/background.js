a92ab6c8b2ea18da6e400107a76674310cfe64f9
awtnb <arikaebanataw@gmail.com>
2024-12-11 16:09:55 +0900

    not button, but menu

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
-const handleButtonClick = async () => {
+const handleMenuClick = async (selectedText) => {
   const mod = await import("./default.js");
 
   const [promptData, llmUrlData] = await Promise.all([browser.storage.local.get("promptInput"), browser.storage.local.get("llmUrl")]);
@@ -6,14 +6,29 @@ const handleButtonClick = async () => {
   const prompt = promptData.promptInput || mod.DEFAULT_PROMPT;
   const llmUrl = llmUrlData.llmUrl || mod.DEFAULT_LLM_URL;
 
-  const selectedText = await messenger.Helper.getSelectedText().catch((err) => {
-    console.error("Error getting selected text:", err);
-    return "";
-  });
-
   await Promise.all([navigator.clipboard.writeText(prompt.replace("{}", selectedText)), browser.windows.openDefaultBrowser(llmUrl)]).catch((err) => {
     console.error(err);
   });
 };
 
-browser.action.onClicked.addListener(handleButtonClick);
+const MENU_ID = "menu-ask-ai";
+
+messenger.menus.removeAll();
+messenger.menus.create(
+  {
+    id: MENU_ID,
+    title: "Copy with prompt and ask AI",
+    contexts: ["selection"],
+  },
+  () => {
+    if (browser.runtime.lastError) {
+      console.error(browser.runtime.lastError);
+    }
+  }
+);
+
+messenger.menus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId === MENU_ID) {
+    handleMenuClick(info.selectionText);
+  }
+});
